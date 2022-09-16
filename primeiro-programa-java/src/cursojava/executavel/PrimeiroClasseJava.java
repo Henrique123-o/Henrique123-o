01cf150df5530fd009bd8249117c2855c7a9128e
hsilv <hsilv@DESKTOP-DNMNQGI>
2022-09-15 22:37:18 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+public class PrimeiroClasseJava {
+	
+	/* Main é um  metodo auto executavel em java*/
+   public static void main(String[] args) {
+	   
+	   /* Excreve texto no console*/
+	   System.out.println("icaro");
+	   System.out.println("icaro");
+		
+	
+  }
+	}
