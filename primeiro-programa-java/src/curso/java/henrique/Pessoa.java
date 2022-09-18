45d6544c6d9d5373043aeb8aae1d525dd8958e28
hsilv <hsilv@DESKTOP-DNMNQGI>
2022-09-18 00:59:49 -0300

    olá

--- primeiro-programa-java/src/curso/java/henrique/Pessoa.java
+++ primeiro-programa-java/src/curso/java/henrique/Pessoa.java
@@ -5,6 +5,28 @@ public class Pessoa {
 	private String nome;
 	
 	private String sobrenome;
+	
+	private int idade;
+	
+    private Date dataNascimento;
+    
+    
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 
 	public String getNome() {
 		return nome;
